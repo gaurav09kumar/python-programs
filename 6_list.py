48782506719c4b6c4f58a2196af40a1f81978d88
Gaurav Kumar Rai <34624495+gaurav09kumar@users.noreply.github.com>
2026-02-07 07:34:54 +0530

    Create 6_list.py

---
+++ 6_list.py
@@ -0,0 +1,8 @@
+grades = [85,92,78,90,88]
+grades.append(56)
+print(f"Grades: {grades}")
+average_grades = sum(grades) / len(grades)
+print(f"Avergae Grades : {average_grades}")
+
+print("Max : ",max(grades))
+print("Min : ", min(grades))

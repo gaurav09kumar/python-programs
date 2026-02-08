1f50627782cd8c5a28ae20de42857481bb555c1b
Gaurav Kumar Rai <34624495+gaurav09kumar@users.noreply.github.com>
2026-02-08 15:33:29 +0530

    Create 29_functions.py

---
+++ 29_functions.py
@@ -0,0 +1,17 @@
+"""
+Line Equation
+Problem Description:
+You are given the slope m and the y-intercept b of a line, along with a value x. Your task is to calculate and return the value of y using the equation of a line in slope-intercept form:
+y=mx+b
+Input:
+Three floating-point numbers: slope, intercept, and x.
+Output:
+A floating-point number representing the value of yyy corresponding to the given xxx.
+Example:
+Input: slope = 2, intercept = 3, x = 4
+Output: 11.0
+Input: slope = 1.5, intercept = -2, x = 2
+Output: 1.0
+"""
+def calculate_lift_rounds(n, capacity):
+    return (n + capacity - 1) // capacity

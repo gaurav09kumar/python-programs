e696d2b5607d7bc4860edaff50a82aab083cd1b4
Gaurav Kumar Rai <34624495+gaurav09kumar@users.noreply.github.com>
2026-02-09 20:53:02 +0530

    Update 3.bubble_sort.py

--- searching_and_sorting/3.bubble_sort.py
+++ searching_and_sorting/3.bubble_sort.py
@@ -1,14 +1,17 @@
-def bubble_sort(arr):
+def bubble_sort(lst):
+    # Your code goes here
+    if len(lst) <= 1:  # Handle empty/single element
+        return lst
     start = 0
-    end = len(arr)-1
-    while(start!=end):
+    end = len(lst)-1
+    while(start<end):
         for i in range(start,end):
-            if arr[i]>arr[i+1]:
-                temp=arr[i]
-                arr[i]=arr[i+1]
-                arr[i+1]=temp
+            if lst[i]>lst[i+1]:
+                temp=lst[i]
+                lst[i]=lst[i+1]
+                lst[i+1]=temp
         end=end-1
-    return arr
+    return lst
 
 print(bubble_sort([12,25,11,34,90,22]))
         

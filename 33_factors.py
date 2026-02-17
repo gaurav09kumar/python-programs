f30bbbb451632d268d189a9fcc4782662982e4a4
Gaurav Kumar Rai <34624495+gaurav09kumar@users.noreply.github.com>
2026-02-17 15:11:10 +0530

    Create 33_factors.py

---
+++ 33_factors.py
@@ -0,0 +1,45 @@
+# Print factors / Divisors
+"""
+10 {1 2 5 10}
+15 {1 3 5}
+25 {1 5 25}
+7 {1 7}
+19 {1 9}
+"""
+def factors(num):
+    ans = [] # Space Complexity - O(k) k would be the total number of factors
+    for i in range(1,num+1): # Time Complexity - O(N)
+        if  num % i == 0: # O(1) So ignore
+            ans.append(i) # O(1) So ignore
+    return ans
+
+print(factors(10))
+print(factors(15))
+print(factors(25))
+
+def factors2(num):
+    ans = [] # Space Complexity - O(k) k would be the total number of factors
+    for i in range(1,num//2+1): # Time Complexity O(N/2) -> O(N * 1/2) -> Almost equal to O(N)
+        if  num % i == 0: # O(1) So ignore
+            ans.append(i) # O(1) So ignore
+    ans.append(num) # O(1) So ignore
+    return ans
+
+print(factors2(10))
+print(factors2(15))
+print(factors2(25))
+
+# optimal
+def factors3(num):
+    ans = [] # Space Complexity - O(k) k would be the total number of factors
+    for i in range(1,int((num)**(1/2))+1): # Time Complexity O(N/2) -> O(Square root of N)
+        if num % i == 0: # O(1) So ignore
+           ans.append(i) # O(1) So ignore
+           if num // i != i:
+               ans.append(num//i) # O(1) So ignore
+        ans.sort()
+    return ans
+
+print(factors3(10))
+print(factors3(15))
+print(factors3(25))
